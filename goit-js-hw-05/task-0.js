9a8306d71248e946121fa304b3b92ebbd0a343bc
smdeile <smdeile@gmail.com>
2019-12-23 15:55:37 +0200

    5hw

--- goit-js-hw-05/task-0.js
+++ goit-js-hw-05/task-0.js
@@ -47,3 +47,184 @@
 //   };
 // };
 // console.log(multiply(10)([2, 10, 15]));
+
+// const multiply = arr => {
+//   const newArr = [];
+//   for (let numb of arr) {
+//     let action = numb * 2;
+//     newArr.push(action);
+//   }
+//   return newArr;
+// };
+// console.log(multiply([1, 2, 3]));
+
+// function highAndLow(numbers) {
+//   const arr = numbers.split(' ');
+//   console.log(arr);
+//   let big;
+//   let small;
+//   for (let item of arr) {
+//     const figure = Number(item);
+//     if (Number(item) > figure) {
+//       big = item;
+//     }
+
+//     if (Number(item) < figure) {
+//       small = item;
+//     }
+//   }
+//   return `${big} -${small}`;
+// }
+// console.log(highAndLow('1 2 3 32 4 5 6'));
+// function highAndLow(numbers){
+//     const arr=numbers.split(' ')
+//     let big
+//     let small
+//     for (let item of arr) {
+//     if (Number(item)>big){
+//     big=item}
+//     if(Number(item)<small) {
+//     small=item}}
+//       return `${big} ${small}`
+
+//   }
+// function noSpace(x) {
+//   let newString = x.replace(/\s/g, '');
+//   return newString;
+// }
+
+// noSpace('1  2 3 5 4 6ff/ f fd s');
+///////////////////////////////////////////////////
+// function likes(names) {
+//   let message = 'no one likes this';
+//   if (names.length === 1) {
+//     message = `${names[0]} like this`;
+//   } else if (names.length === 2) {
+//     message = `${names[0]} and ${names[1]} like this`;
+//   } else if (names.length === 3) {
+//     message = `${names[0]}, ${names[1]} and ${names[2]} like this`;
+//   } else if (names.length >= 4) {
+//     message = `${names[0]}, ${names[1]} and ${names.length -
+//       2} others like this`;
+//   }
+
+//   console.log(message);
+// }
+// likes(['Alex', 'Jacob', 'Mark', 'Max', 'Alex', 'Jacob', 'Mark', 'Max']),
+//   'Alex, Jacob and 2 others like this';
+////////////////////////////////////////////////////////////
+// function well(x) {
+//   let good = 0;
+//   // x = [];
+//   let message = 'Fail!';
+//   for (let item of x) {
+//     if (item === 'good') {
+//       good++;
+//     }
+//     if (good === 1 || good === 2) {
+//       message = 'Publish!';
+//     }
+//     if (good > 2) {
+//       message = 'I smell a series!';
+//     }
+//   }
+
+//   return message;
+// }
+// console.log(
+//   well(['good', 'bad', 'bad', 'bad', 'bad', 'good', 'bad', 'bad', 'good']),
+// );
+// console.log(well(['bad', 'bad', 'bad']));
+// console.log(well(['good', 'bad', 'bad', 'bad', 'bad']));
+// console.log(well([]));
+////////////////////////////////////////////////////////////////
+// const inventory = {
+//   items: ['Knife', 'Gas mask'],
+//   add(itemName) {
+//     console.log(`Adding ${itemName} to inventory`);
+
+//     this.items.push(itemName);
+//   },
+//   remove(itemName) {
+//     console.log(`Removing ${itemName} from inventory`);
+
+//     this.items = this.items.filter(item => item !== itemName);
+//   },
+// };
+
+// const invokeInventoryAction = function(itemName, action) {
+//   console.log(`Invoking action on ${itemName}`);
+//   action(itemName);
+// };
+
+// invokeInventoryAction('Medkit', inventory.add);
+// // Invoking action on Medkit
+// // Adding Medkit to inventory
+
+// console.log(inventory.items); // ['Knife', 'Gas mask', 'Medkit']
+
+// invokeInventoryAction('Gas mask', inventory.remove);
+// // Invoking action on Gas mask
+// // Removing Gas mask from inventory
+
+// console.log(inventory.items); // ['Knife', 'Medkit']
+// class Store {
+//   constructor(name, description) {
+//     this.name = name;
+//     this.description = description;
+//   }
+//   editName() {
+//     if (!user.rights) {
+//       console.log('error');
+//     }
+//   }
+// }
+// const store = {
+//   name: 'Nike',
+//   description: 'lorem ipsum',
+// };
+// class User {
+//   constructor(name, salary, age) {
+//     this.name = name;
+//     this.salary = salary;
+//     this.age = age;
+//     this.rights = false;
+//   }
+// }
+// class Admin extends User {
+//   constructor(name, salary, age) {}
+// }
+// const store = new Store('Nike', 'lorem ipsum');
+// store.editName('sndfsk', user);
+// store.editName('snsdfsk', admin);
+//
+class Car {
+  constructor({ maxSpeed, speed = 0 }) {
+    this._maxSpeed = maxSpeed;
+    this._speed = speed;
+  }
+
+  getCar() {
+    return this._maxSpeed;
+  }
+  getSpeed() {
+    return this._speed;
+  }
+  get maxSpeed() {
+    return this._maxSpeed;
+  }
+  get speed() {
+    return this._speed;
+  }
+  set maxSpeed(km) {
+    this._maxSpeed = km;
+  }
+}
+const zapor = new Car({
+  maxSpeed: 150,
+});
+console.log(zapor.getCar());
+console.log(zapor.maxSpeed);
+zapor.maxSpeed = 200;
+console.log(zapor.maxSpeed);
+console.log(zapor.speed);
--- goit-js-hw-05/task-2.js
+++ goit-js-hw-05/task-2.js
@@ -1,30 +1,28 @@
-const inventory = {
-  items: ['Knife', 'Gas mask'],
-  add(itemName) {
-    console.log(`Adding ${itemName} to inventory`);
-
-    this.items.push(itemName);
-  },
-  remove(itemName) {
-    console.log(`Removing ${itemName} from inventory`);
-
-    this.items = this.items.filter(item => item !== itemName);
-  },
+const User = function({ name, age, followers }) {
+  (this.name = name), (this.age = age), (this.followers = followers);
+  // this.getInfo = function() {
+  //   return console.log(
+  //     `User ${this.name} is ${this.age} years old and has ${this.followers} followers`,
+  //   );
+  // };
 };
-
-const invokeInventoryAction = function(itemName, action) {
-  console.log(`Invoking action on ${itemName}`);
-  action(itemName);
+User.prototype.getInfo = function() {
+  return console.log(
+    `User ${this.name} is ${this.age} years old and has ${this.followers} followers`,
+  );
 };
+const mango = new User({
+  name: 'Mango',
+  age: 2,
+  followers: 20,
+});
 
-invokeInventoryAction('Medkit', inventory.add);
-// Invoking action on Medkit
-// Adding Medkit to inventory
+mango.getInfo(); // User Mango is 2 years old and has 20 followers
 
-console.log(inventory.items); // ['Knife', 'Gas mask', 'Medkit']
+const poly = new User({
+  name: 'Poly',
+  age: 3,
+  followers: 17,
+});
 
-invokeInventoryAction('Gas mask', inventory.remove);
-// Invoking action on Gas mask
-// Removing Gas mask from inventory
-
-console.log(inventory.items); // ['Knife', 'Medkit']
+poly.getInfo(); // User Poly is 3 years old and has 17 followers
