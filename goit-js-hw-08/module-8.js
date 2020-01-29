37a72ad24c58ddbc07ce6573f4e77e18b1566fe7
smdeile <smdeile@gmail.com>
2020-01-29 21:15:48 +0200

    8hw 1cmmit

--- goit-js-hw-08/module-8.js
+++ goit-js-hw-08/module-8.js
@@ -4,6 +4,7 @@ const gallery = document.querySelector('.js-gallery');
 const modal = document.querySelector('.js-lightbox');
 const lightBoxOpenImage = document.querySelector('.lightbox__image');
 const closeButton = document.querySelector('.lightbox__button');
+const Overlay = document.querySelector('.lightbox__content');
 function createMarkup(items) {
   const markup = items.reduce((markup, user) => {
     const { preview, original, description } = user;
@@ -36,6 +37,37 @@ const handleClickButton = function(e) {
   modal.classList.toggle('is-open');
   lightBoxOpenImage.setAttribute('src', '');
 };
+const handleClickOverlay = function(e) {
+  if (e.target === e.currentTarget) {
+    modal.classList.toggle('is-open');
+    lightBoxOpenImage.setAttribute('src', '');
+  }
+  console.log(e.currentTarget);
+};
+const keyboard = function(e) {
+  if (e.key === 'Escape') {
+    modal.classList.toggle('is-open');
+    lightBoxOpenImage.setAttribute('src', '');
+  }
+  if (e.key === 'ArrowLeft') {
+    items.map(({ original }, indx) => {
+      if ((original === lightBoxOpenImage.getAttribute('src')) & (indx !== 0)) {
+        return lightBoxOpenImage.setAttribute('src', items[indx - 1].original);
+      }
+    });
+  }
+  if (e.key === 'ArrowRight') {
+    items.map(({ original }, indx) => {
+      if (original === lightBoxOpenImage.getAttribute('src')) {
+        console.log(items[indx]);
+        console.log(original);
+        return lightBoxOpenImage.setAttribute('src', items[indx + 1].original);
+      }
+    });
+  }
+};
 closeButton.addEventListener('click', handleClickButton);
 gallery.addEventListener('click', handleClickImage);
+Overlay.addEventListener('click', handleClickOverlay);
+window.addEventListener('keyup', keyboard);
 createMarkup(items);
