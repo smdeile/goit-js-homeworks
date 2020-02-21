7a20b3d38b370fb089f876606860267ff702000d
smdeile <smdeile@gmail.com>
2020-02-21 15:24:38 +0200

    11 add coloriser

---
+++ goit-js-hw-11-color-switch/index.js
@@ -0,0 +1,54 @@
+import colors from './colors.js';
+const refs = {
+  body: document.querySelector('body'),
+  startBtn: document.querySelector('[data-action="start"]'),
+  stopBtn: document.querySelector('[data-action="stop"]'),
+};
+// let timerId = null;
+// let isActive = false;
+// const randomIntegerFromInterval = (min, max) => {
+//   return Math.floor(Math.random() * (max - min + 1) + min);
+// };
+// const changeColor = function() {
+//   refs.body.style.backgroundColor = colors[randomIntegerFromInterval(0, 5)];
+// };
+// const startChangeColor = function() {
+//   if (isActive == false) {
+//     timerId = setInterval(changeColor, 1000);
+//     isActive = true;
+//     console.log('true');
+//   }
+// };
+// const stopChangeColor = function() {
+//   clearInterval(timerId);
+//   isActive = false;
+// };
+const randomiserColor = {
+  isActive: false,
+  changerColor() {
+    const randomIntegerFromInterval = (min, max) => {
+      return Math.floor(Math.random() * (max - min + 1) + min);
+    };
+    refs.body.style.backgroundColor = colors[randomIntegerFromInterval(0, 5)];
+  },
+
+  startChangeColor() {
+    if (this.isActive === true) {
+      return;
+    }
+    this.intervalId = setInterval(this.changerColor, 1000);
+    this.isActive = true;
+  },
+  stopChangeColor() {
+    clearInterval(this.intervalId);
+    this.isActive = false;
+  },
+};
+refs.startBtn.addEventListener(
+  'click',
+  randomiserColor.startChangeColor.bind(randomiserColor),
+);
+refs.stopBtn.addEventListener(
+  'click',
+  randomiserColor.stopChangeColor.bind(randomiserColor),
+);
